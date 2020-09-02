5f981a25c3e9b9a312be8439302e42c59cecc72d
number 1 dev <67679722+spartenwolffe@users.noreply.github.com>
2020-09-02 08:23:10 +0100

    Removed Useless code.

--- src/java/main/Swing GUI/app.java
+++ src/java/main/Swing GUI/app.java
@@ -35,8 +35,6 @@ public class app{
         send_text.setBounds(600, 520, 100, 70);
         send_text.addActionListener(action -> {
             JOptionPane.showMessageDialog(frame.getComponent(0), message_box.getText());
-            JLabel message = new JLabel(message_box.getText());
-            message.setBounds(0, 200, 200, 100);
             send_text.setVisible(true);
         });
                 panel.add(send_text);
